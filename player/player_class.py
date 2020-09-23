e2a2df35dac3e3b85d6477ed77d68b9822a1b9c3
chris-greening <chris@christophergreening.com>
2020-09-23 17:10:31 -0400

    Added player class types

---
+++ player/player_class.py
@@ -0,0 +1,16 @@
+class _PlayerClass:
+    def __init__(self, hp, mp):
+        self.hp = hp 
+        self.mp = mp 
+
+class Mage:
+    def __init__(self):
+        self.base_hp =  
+
+class Knight:
+    def __init__(self):
+        pass 
+
+class Archer:
+    def __init__(self):
+        pass 
